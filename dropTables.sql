adb75592c007401235e60c98e0b2ed60ab6402b0
jlevinpool <poolj@onid.oregonstate.edu>
2016-02-28 22:09:45 -0800

    Schema Table Working
    Also added dropTables

---
+++ dropTables.sql
@@ -0,0 +1,20 @@
+--
+-- Author: James Pool
+-- ONID: 932664412
+-- OSU Email: poolj@oregonstate.edu
+-- Initial Creation: 28 February 2016
+--
+-- Filename: dropTables.sql
+-- Description: Drops all tables related to mlp-fim (allows starting from clean)
+-- 
+
+SET FOREIGN_KEY_CHECKS = 0;
+DROP TABLE IF EXISTS `mlp_location`;
+DROP TABLE IF EXISTS `mlp_occupation`;
+DROP TABLE IF EXISTS `mlp_pony`;
+DROP TABLE IF EXISTS `mlp_ponyChild`;
+DROP TABLE IF EXISTS `mlp_ponyLocation`;
+DROP TABLE IF EXISTS `mlp_ponyOccupation`;
+DROP TABLE IF EXISTS `mlp_ponyType`;
+DROP TABLE IF EXISTS `mlp_type`;
+SET FOREIGN_KEY_CHECKS = 1;
