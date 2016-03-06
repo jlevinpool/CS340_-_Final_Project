18bd356168a9c71b27d71056e7b7f453f32d7443
jlevinpool <poolj@onid.oregonstate.edu>
2016-03-05 18:26:51 -0800

    Updated file names and fixed FK in data

--- mlp-fim-data.sql
+++ mlp-fim-data.sql
@@ -8,52 +8,48 @@
 -- Description: Initial data creation document for final project
 -- 
 
+SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0;
+SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0;
+SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='TRADITIONAL';
+
 --
 -- Data dump for table 'mlp_pony'
 --
 
 SET AUTOCOMMIT=0;
-INSERT INTO mlp_pony (id, name, gender, cutieMark) VALUES (
-	(1,'TWILIGHT SPARKLE','F','Pink six-pointed sparkle surrounded by five small white sparkles'),
-	(2,'APPLEJACK','F','Three red apples'),
-	(3,'FLUTTERSHY','F','Three butterflies with cyan bodies and pink wings'),
-	(4,'RARITY','F','Three blue lozenge diamonds'),
-	(5,'PINKIE PIE','F','Two blue balloons with yellow strings and one yellow balloon with a blue string'),
-	(6,'RAINBOW DASH','F','Blue-yellow-red lighting bolt with cloud')
-);
+INSERT INTO mlp_pony (id, name, gender, cutieMark, typeID) VALUES
+	(1,'TWILIGHT SPARKLE','F','Pink six-pointed sparkle surrounded by five small white sparkles',1),
+	(2,'APPLEJACK','F','Three red apples',2),
+	(3,'FLUTTERSHY','F','Three butterflies with cyan bodies and pink wings',3),
+	(4,'RARITY','F','Three blue lozenge diamonds',4),
+	(5,'PINKIE PIE','F','Two blue balloons with yellow strings and one yellow balloon with a blue string',2),
+	(6,'RAINBOW DASH','F','Blue-yellow-red lighting bolt with cloud',3);
 COMMIT;
 
 --
 -- Data dump for table 'mlp_type'
 --
 SET AUTOCOMMIT=0;
-INSERT INTO mlp_type (id, name, wings, horn) VALUES (
+INSERT INTO mlp_type (id, name, wings, horn) VALUES
 	(1,'ALICORN',TRUE,TRUE),
 	(2,'EARTH',FALSE,FALSE),
 	(3,'PEGASUS',TRUE,FALSE),
-	(4,'UNICORN',FALSE,TRUE),
-);
+	(4,'UNICORN',FALSE,TRUE);
 COMMIT;
 
 --
 -- Data dump for table 'mlp_location'
 --
 SET AUTOCOMMIT=0;
-INSERT INTO mlp_location (id, name) VALUES (
+INSERT INTO mlp_location (id, name) VALUES
 	(1,'PONYVILLE'),
 	(2,'CANTERLOT'),
 	(3,'CLOUDSDALE'),
 	(4,'CRYSTAL EMPIRE'),
 	(5,'EVERFREE FOREST'),
-	(6,'MANEHATTAN')	
-);
+	(6,'MANEHATTAN');
 COMMIT;
 
 --
 -- Data dump for table 'mlp_occupation'
 --
-SET AUTOCOMMIT=0;
-INSERT INTO mlp_occupation (
-	(1,)
-);
-COMMIT;
